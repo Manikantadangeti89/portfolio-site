b630fea727a633dd07071044bf3a090fe6410e4c
Manikanta Dangeti <manikantadangeti89@gmail.com>
2025-04-25 09:44:04 +0530

    Updated the image and updated projects section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import MyImage from "../assets/images/MyImageGibli.png";
+import MyImage from "../assets/images/MyImage.jpg";
 import "../styles/AboutMe.css";
 
 const AboutMe = () => {
@@ -15,7 +15,7 @@ const AboutMe = () => {
       </div>
       <div className="about-right">
         <h1>About Me</h1>
-        <p>
+        {/* <p>
           Hi! I'm Manikanta Dangeti, a passionate full-stack developer who loves building web
           applications that solve real-world problems. I’m enthusiastic about technology and
           constantly learning new tools and frameworks to improve my craft.
@@ -24,7 +24,21 @@ const AboutMe = () => {
           I specialize in the MERN stack, and I have experience creating clean UI designs,
           integrating with APIs, and managing databases. Apart from coding, I enjoy solving puzzles
           and diving into competitive programming challenges.
-        </p>
+        </p> */}
+        <p>Hi, I’m Manikanta Dangeti — a full-stack developer who’s as comfortable writing backend logic as 
+            I am crafting intuitive frontends.My journey started with curiosity, grew through consistency, 
+            and continues with a deep hunger to build meaningful digital experiences.
+
+            <br/><br/>Over the years, I’ve built a range of projects — from recipe-sharing platforms to blood donation systems
+            to rental websites — not because I had to, but because I wanted to create something real. 
+            I’m fluent in React, Node.js, Express, MongoDB, Java, and SQL, and I’m always expanding my toolkit.
+
+            <br/><br/>I value clean code, user-focused design, and continuous iteration.  
+            But more than that, I value growth through action, even when it's messy.
+            I'm not here just to get hired. I'm here to build, share, and make a mark.  
+            If you're looking for someone who blends creative energy with technical execution — let's connect.
+
+            <br/><br/>📍 Always building. Always learning. Always leveling up.</p>
         <div className="about-buttons">
           <Link to="/resume" className="about-btn">Resume</Link>
           <Link to="/projects" className="about-btn">Projects</Link>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,24 +4,50 @@ import momNoteImg from '../assets/images/momsnote.avif';
 import bloodImg from '../assets/images/bdms1.jpg';
 import rentalImg from '../assets/images/findUrNest2.jpg';
 
+
 const Projects = () => {
+  const p1 = {
+    title : "Mom's Note – Recipe Sharing Website",
+    description : `A feature-rich recipe sharing platform designed for food lovers to explore, upload, and save their favorite recipes.
+              Users can browse trending recipes from an external API as well as community-submitted ones stored in a MongoDB database.
+              It offers a sleek UI with filtering, search functionality, and real-time form validation for submissions.
+              The design mimics a mom’s guidance, offering helpful cooking notes and tips throughout.
+              Responsive and mobile-friendly with React routing, reusable components, and state management.
+              Built using MERN stack with integration of the Spoonacular API for added variety.`,
+    techstack : " React, Node.js, Express, MongoDB, Spoonacular API"
+  }
+  const p2 = {
+    title : "Blood Donation Management System",
+    description : `This web application streamlines the process of managing blood donations and inventory in hospitals and blood banks.
+              Admins can register donors, monitor blood availability by type, and view patient requests in real-time.
+              It has a secure login system, dynamic tables, and alerts for low inventory levels.
+              The UI is clean, intuitive, and responsive with clear call-to-actions for all users.
+              Developed using core PHP and MySQL, with structured backend logic for scalability.
+              Ideal for small clinics or NGOs to digitally manage blood drives and donations.`,
+    techstack : " PHP, MySQL, HTML, CSS"
+  }
+  const p3 = {
+    title : "FindURNest – Rental Booking Website",
+    description : `A fully functional rental booking website allowing users to discover and reserve properties across different locations.
+              Includes user authentication, property filtering, booking confirmations, and owner dashboards.
+              Built with modern UI/UX principles ensuring ease of navigation and responsiveness on all devices.
+              Utilizes RESTful APIs to handle backend operations like user booking, listings, and feedback.
+              Clean database structure to manage users, listings, and booking history with MongoDB.
+              An ideal project to showcase full-stack proficiency using Node.js, Express, and MongoDB.`,
+    techstack : " HTML, CSS, JavaScript, Node.js, Express, MongoDB"
+  }
+
+
   return (
     <div className="projects-container">
       <h1 className="projects-heading">Projects</h1>
 
       <div className="project-card">
-        <h2 className="project-title">Mom's Note – Recipe Sharing Website</h2>
+        <h2 className="project-title">{p1.title}</h2>
         <div className="project-content">
           <div className="project-description">
-            <p>
-              A feature-rich recipe sharing platform designed for food lovers to explore, upload, and save their favorite recipes.
-              Users can browse trending recipes from an external API as well as community-submitted ones stored in a MongoDB database.
-              It offers a sleek UI with filtering, search functionality, and real-time form validation for submissions.
-              The design mimics a mom’s guidance, offering helpful cooking notes and tips throughout.
-              Responsive and mobile-friendly with React routing, reusable components, and state management.
-              Built using MERN stack with integration of the Spoonacular API for added variety.
-            </p>
-            <p><strong>Tech Stack:</strong> React, Node.js, Express, MongoDB, Spoonacular API</p>
+            <p>{p1.description}</p>
+            <p><strong>Tech Stack:</strong>{p1.techstack}</p>
             <div className="project-buttons">
               <a href="https://github.com/Manikantadangeti89/Mom-s-Note---Recipe-Sharing" target="_blank" rel="noopener noreferrer" className="btn">GitHub</a>
               <a href="https://manikantadangeti89.github.io/Mom-s-Note---Recipe-Sharing/" target="_blank" rel="noopener noreferrer" className="btn">View Live</a>
@@ -34,18 +60,11 @@ const Projects = () => {
       </div>
 
       <div className="project-card">
-        <h2 className="project-title">Blood Donation Management System</h2>
+        <h2 className="project-title">{p2.title}</h2>
         <div className="project-content">
           <div className="project-description">
-            <p>
-              This web application streamlines the process of managing blood donations and inventory in hospitals and blood banks.
-              Admins can register donors, monitor blood availability by type, and view patient requests in real-time.
-              It has a secure login system, dynamic tables, and alerts for low inventory levels.
-              The UI is clean, intuitive, and responsive with clear call-to-actions for all users.
-              Developed using core PHP and MySQL, with structured backend logic for scalability.
-              Ideal for small clinics or NGOs to digitally manage blood drives and donations.
-            </p>
-            <p><strong>Tech Stack:</strong> PHP, MySQL, HTML, CSS</p>
+            <p>{p2.description}</p>
+            <p><strong>Tech Stack:</strong>{p2.techstack}</p>
             <div className="project-buttons">
               <a href="https://github.com/Manikantadangeti89/blood-donation-management-system" target="_blank" rel="noopener noreferrer" className="btn">GitHub</a>
               <a href="#" className="btn">View Live</a>
@@ -58,18 +77,11 @@ const Projects = () => {
       </div>
 
       <div className="project-card">
-        <h2 className="project-title">FindURNest – Rental Booking Website</h2>
+        <h2 className="project-title">{p3.title}</h2>
         <div className="project-content">
           <div className="project-description">
-            <p>
-              A fully functional rental booking website allowing users to discover and reserve properties across different locations.
-              Includes user authentication, property filtering, booking confirmations, and owner dashboards.
-              Built with modern UI/UX principles ensuring ease of navigation and responsiveness on all devices.
-              Utilizes RESTful APIs to handle backend operations like user booking, listings, and feedback.
-              Clean database structure to manage users, listings, and booking history with MongoDB.
-              An ideal project to showcase full-stack proficiency using Node.js, Express, and MongoDB.
-            </p>
-            <p><strong>Tech Stack:</strong> HTML, CSS, JavaScript, Node.js, Express, MongoDB</p>
+            <p>{p2.description}</p>
+            <p><strong>Tech Stack:</strong>{p3.techstack}</p>
             <div className="project-buttons">
               <a href="https://github.com/Manikantadangeti89/FindURNest" target="_blank" rel="noopener noreferrer" className="btn">GitHub</a>
               <a href="#" className="btn">View Live</a>
